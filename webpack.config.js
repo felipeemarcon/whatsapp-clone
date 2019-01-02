37ea2204c8f6bafc62203b0c027dd4286b48b73a
Felipe Marcon <felipeemarcon@gmail.com>
2019-01-02 14:13:09 -0200

    Import and Export modules with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'budle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, '/dist'),
     publicPath: 'dist'
   }
