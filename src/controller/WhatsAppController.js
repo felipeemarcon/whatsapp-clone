13a74430b71a91bf82d9b5f52fdfc2db53c78c25
Felipe Marcon <felipeemarcon@gmail.com>
2019-01-04 18:45:01 -0200

    Create conversation and using btoa() function (convert to base64)

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from './../util/Firebase';
 import { User } from '../model/User';
+import { Chat } from '../model/Chat';
 
 export class WhatsAppController {
   constructor(){
@@ -202,13 +203,25 @@ export class WhatsAppController {
 
         if (data.name) {
 
-          this._user.addContact(contact).then(() => {
+          Chat.createIfNotExists(this._user.email, contact.email).then(chat => {
 
-            this.el.btnClosePanelAddContact.click();
+            contact.chatId = chat.id;
 
-            console.info('Contato foi adicionado!');
+            this._user.chatId = chat.id;
+
+            contact.addContact(this._user);
+
+            this._user.addContact(contact).then(() => {
+
+              this.el.btnClosePanelAddContact.click();
+
+              console.info('Contato foi adicionado!');
+
+            });
 
           });
+
+         
           
         } else {
 
@@ -599,6 +612,8 @@ export class WhatsAppController {
         }
 
         div.on('click', e => {
+
+          console.log('Chat id', contact.chatId);
 
           this.el.activeName.innerHTML = contact.name;
           this.el.activeStatus.innerHTML = contact.status;
